2296c6b8622a7ea19b7d34e5c33a4f1fa5aee64d
c-kocheise <72945618+c-kocheise@users.noreply.github.com>
2022-10-13 13:51:05 +0200

    TS und JS Datei erstellen, Array anlegen

---
+++ A01_RandomPoem/PoemTS.js
@@ -0,0 +1,14 @@
+"use strict";
+var A01_Poem;
+(function (A01_Poem) {
+    let subjects = ["Harry", "Hermine", "Ron", "Dumbledore", "Snape"];
+    let predicates = ["dances", "cries", "laughs", "talks", "performs"];
+    let objects = ["with Hagrid.", "in Hogsmeade.", "in the great hall.", "with the headless Nick.", "against the dark arts."];
+    console.log(subjects);
+    console.log(predicates);
+    console.log(objects);
+})(A01_Poem || (A01_Poem = {}));
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+}
+//# sourceMappingURL=PoemTS.js.map
---
+++ A01_RandomPoem/PoemTS.ts
@@ -0,0 +1,12 @@
+namespace A01_Poem{
+    let subjects: string[] = ["Harry", "Hermine", "Ron", "Dumbledore", "Snape"];
+    let predicates: string[] = ["dances", "cries", "laughs", "talks", "performs"];
+    let objects: string[] = ["with Hagrid.", "in Hogsmeade.", "in the great hall.", "with the headless Nick.", "against the dark arts."];
+    console.log(subjects);
+    console.log(predicates);
+    console.log(objects);
+}
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
