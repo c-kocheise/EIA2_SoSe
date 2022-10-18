3d72bc99e7ff6305df1c5ad164d1c1d6bc12da84
c-kocheise <72945618+c-kocheise@users.noreply.github.com>
2022-10-18 17:40:56 +0200

    Create Random Poem

--- A01_RandomPoem/PoemTS.js
+++ A01_RandomPoem/PoemTS.js
@@ -4,11 +4,26 @@ var A01_Poem;
     let subjects = ["Harry", "Hermine", "Ron", "Dumbledore", "Snape"];
     let predicates = ["dances", "cries", "laughs", "talks", "performs"];
     let objects = ["with Hagrid.", "in Hogsmeade.", "in the great hall.", "with the headless Nick.", "against the dark arts."];
-    console.log(subjects);
-    console.log(predicates);
-    console.log(objects);
+    // console.log(subjects);
+    // console.log(predicates);
+    // console.log(objects);
+    for (let index = subjects.length; index >= 1; index--) {
+        // console.log(index);
+        let versAusgabe = getVerse(subjects, predicates, objects);
+        console.log(versAusgabe);
+    }
+    function getVerse(_subjects, _predicates, _objects) {
+        // console.log("Hallo ich bin die Funktion");
+        let randomNumber1 = Math.floor(Math.random() * _subjects.length);
+        let randomNumber2 = Math.floor(Math.random() * _predicates.length);
+        let randomNumber3 = Math.floor(Math.random() * _objects.length);
+        // console.log(randomNumber);
+        let randomSubject = _subjects.splice(randomNumber1, 1);
+        let randomPredicate = _predicates.splice(randomNumber2, 1);
+        let randomObject = _objects.splice(randomNumber3, 1);
+        let vers = randomSubject[0] + " " + randomPredicate[0] + " " + randomObject[0];
+        //Durch Splice wurde ein Wort aus dem Array Herausgeschnitten, dadurch wird dieses Wort aus dem Array herausgenommen.
+        return vers;
+    }
 })(A01_Poem || (A01_Poem = {}));
-for (let index = 0; index < array.length; index++) {
-    const element = array[index];
-}
 //# sourceMappingURL=PoemTS.js.map
--- A01_RandomPoem/PoemTS.ts
+++ A01_RandomPoem/PoemTS.ts
@@ -1,12 +1,31 @@
-namespace A01_Poem{
+namespace A01_Poem {
     let subjects: string[] = ["Harry", "Hermine", "Ron", "Dumbledore", "Snape"];
     let predicates: string[] = ["dances", "cries", "laughs", "talks", "performs"];
     let objects: string[] = ["with Hagrid.", "in Hogsmeade.", "in the great hall.", "with the headless Nick.", "against the dark arts."];
-    console.log(subjects);
-    console.log(predicates);
-    console.log(objects);
+    // console.log(subjects);
+    // console.log(predicates);
+    // console.log(objects);
+
+    for (let index: number = subjects.length; index >= 1; index--) {
+        // console.log(index);
+        let versAusgabe: string = getVerse(subjects, predicates, objects);
+        console.log(versAusgabe);
+    }
+    function getVerse(_subjects: string[], _predicates: string[], _objects: string[]): string {
+        
+        // console.log("Hallo ich bin die Funktion");
+
+        let randomNumber1: number = Math.floor(Math.random() * _subjects.length);
+        let randomNumber2: number = Math.floor(Math.random() * _predicates.length);
+        let randomNumber3: number = Math.floor(Math.random() * _objects.length);
+        // console.log(randomNumber);
+        let randomSubject: string[] = _subjects.splice(randomNumber1, 1);
+        let randomPredicate: string[] = _predicates.splice(randomNumber2, 1);
+        let randomObject: string[] = _objects.splice(randomNumber3, 1);
+        let vers: string = randomSubject[0] + " " + randomPredicate[0] + " " + randomObject[0];
+        //Durch Splice wurde ein Wort aus dem Array Herausgeschnitten, dadurch wird dieses Wort aus dem Array herausgenommen.
+        return vers;
+    }
+
 }
-for (let index = 0; index < array.length; index++) {
-    const element = array[index];
-    
-}
+
