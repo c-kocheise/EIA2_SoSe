0bbd0ed206e9ec11bb06b2cc79f4844ce51a50bd
c-kocheise <72945618+c-kocheise@users.noreply.github.com>
2022-10-20 12:59:56 +0200

    ADD Eventlistener

---
+++ A02_EventInspector/InspectorTS.ts
@@ -0,0 +1,23 @@
+namespace A02_EventInspector {
+window.addEventListener("load", handleLoad);
+
+function handleLoad (): void{
+    let div0: HTMLDivElement = <HTMLDivElement> document.querySelector(".div0");
+    let div1: HTMLDivElement = <HTMLDivElement> document.querySelector(".div1");
+    // <HTMLDivElement> ist für Fehleranzeige, Wenn div Element null sein kann, man sich aber sicher ist, dass es existiert
+    document.addEventListener("mousemove", setInfobox)
+    document.addEventListener("click", logInfo)
+    document.addEventListener("keyup", logInfo)
+    div0.addEventListener("click", logInfo)
+    div0.addEventListener("keyup", logInfo)
+    div1.addEventListener("click", logInfo)
+    div1.addEventListener("keyup", logInfo)
+    document.body.addEventListener("keyup", logInfo)
+    document.body.addEventListener("keyup", logInfo)
+}
+
+function setInfobox (): void{
+
+}
+
+}
