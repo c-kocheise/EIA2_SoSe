7bd01bc1f79bec180940929e9537f73556046e24
c-kocheise <72945618+c-kocheise@users.noreply.github.com>
2022-10-29 18:46:12 +0200

    Eventinspector Change

--- A03_ShoppingList/ShoppingScript.js
+++ A03_ShoppingList/ShoppingScript.js
@@ -1,2 +1,22 @@
 "use strict";
+var ShoppingHelper;
+(function (ShoppingHelper) {
+    window.addEventListener("load", assignListeners);
+    function assignListeners() {
+        document.getElementById("add_newItem").addEventListener("click", addNewItem);
+        for (let i = 1; i < 4; i++) {
+            document.getElementById("button_status_item_" + i).addEventListener("click", switchStatusEntry);
+            document.getElementById("trash_button_item_" + i).addEventListener("click", logDeleteEntry);
+        }
+    }
+    function addNewItem() {
+        console.log("Neues Item wird hinzugefügt.");
+    }
+    function logDeleteEntry() {
+        console.log("Item wird aus er Liste gelöscht");
+    }
+    function switchStatusEntry() {
+        console.log("Status des Items wird durchgewechselt von: Noch offen -> Im Wagen -> Eingekauft, wieder zu Noch Offen");
+    }
+})(ShoppingHelper || (ShoppingHelper = {}));
 //# sourceMappingURL=ShoppingScript.js.map
---
+++ A03_ShoppingList/ShoppingScript.ts
@@ -0,0 +1,30 @@
+namespace ShoppingHelper 
+{
+    window.addEventListener("load", assignListeners);
+    function assignListeners(): void
+    {
+        document.getElementById("add_newItem").addEventListener("click", addNewItem);
+        for (let i: number = 1; i < 4; i++ )
+        {
+            document.getElementById("button_status_item_" + i).addEventListener("click", switchStatusEntry);
+            document.getElementById("trash_button_item_" + i).addEventListener("click", logDeleteEntry);
+        }
+            
+    }
+    function addNewItem(): void 
+    {
+        console.log("Neues Item wird hinzugefügt.");
+    }
+
+    function logDeleteEntry(): void 
+    {
+        console.log("Item wird aus er Liste gelöscht");
+    }
+
+    function switchStatusEntry(): void 
+    {
+        console.log("Status des Items wird durchgewechselt von: Noch offen -> Im Wagen -> Eingekauft, wieder zu Noch Offen");
+    }
+
+
+}
